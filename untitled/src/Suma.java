c876b357cae33d9911edefd20b1170d5dbd89f27
jesusdiazA <jesusdiaz4toc@gmail.com>
2020-03-12 16:04:21 +0100

    first commit

---
+++ untitled/src/Suma.java
@@ -0,0 +1,28 @@
+public class Suma {
+    private int a, b;
+
+    public Suma(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int totalSuma(int a, int b){
+        return a + b;
+    }
+}
